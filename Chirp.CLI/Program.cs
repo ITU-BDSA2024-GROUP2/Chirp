c369d78d5ae09671f6efe4cdf4b5d6b46ec019ce
Nikolai Tilgreen Nielsen <nikolai@AirtilhNikolai2.lan>
2024-08-28 22:03:27 +0200

    Added method for displaying cheeps.

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -10,23 +10,31 @@ class Program
 
     private static void parseCSV(string path)
     {
+        //Source: https://dev.to/bristolsamo/c-csv-parser-step-by-step-tutorial-25ok
+        
         TextFieldParser csvParser = new TextFieldParser(path);
         csvParser.SetDelimiters(",");
         csvParser.HasFieldsEnclosedInQuotes = true;
+        csvParser.ReadLine(); // skips the first row with column names
 
+        int i = 0;
         while (!csvParser.EndOfData)
         {
-            string[] rows = csvParser.ReadFields();
-            displayCheep(rows);
-            
+            string[] row = csvParser.ReadFields();
+            displayCheep(row);
+
         }
     }
 
-    private static void displayCheep(string[] rows)
+    private static void displayCheep(string[] row)
     {
-        foreach (String cheep in rows)
-        {
-            Console.WriteLine(cheep);
-        }
+        var author = row[0];
+        var message = row[1];
+        
+        var unixTimeStamp = int.Parse(row[2]);
+        DateTimeOffset dateTime = DateTimeOffset.FromUnixTimeSeconds(unixTimeStamp).DateTime;
+        string formattedTime = dateTime.ToString("dd'/'MM'/'yy HH':'mm':'ss");
+        
+        Console.WriteLine(author + " @ " + formattedTime + ": " + message);
     }
 }
