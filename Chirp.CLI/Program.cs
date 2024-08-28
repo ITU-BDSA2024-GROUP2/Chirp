f8ab13bd809069be907ff75797fda74a7231e9a9
Nikolai Tilgreen Nielsen <nikolai@AirtilhNikolai2.lan>
2024-08-28 21:40:05 +0200

    Initial csv parser

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.VisualBasic.FileIO;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        var path = "/Users/nikolai/Chirp/Chirp.CLI/data/chirp_cli_db.csv";
+        parseCSV(path);
+    }
+
+    private static void parseCSV(string path)
+    {
+        TextFieldParser csvParser = new TextFieldParser(path);
+        csvParser.SetDelimiters(",");
+        csvParser.HasFieldsEnclosedInQuotes = true;
+
+        while (!csvParser.EndOfData)
+        {
+            string[] rows = csvParser.ReadFields();
+            displayCheep(rows);
+            
+        }
+    }
+
+    private static void displayCheep(string[] rows)
+    {
+        foreach (String cheep in rows)
+        {
+            Console.WriteLine(cheep);
+        }
+    }
+}
