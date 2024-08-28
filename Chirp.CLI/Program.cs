fc4e97f49099d7453383c9addcc7b3125be10883
Anders0106 <andersgfh@gmail.com>
2024-08-28 15:10:51 +0200

    skift til net7

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -1,6 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-
-
-Console.WriteLine("Hej, Jeg glæder mig til at bestå eksamen");
-// This is a change by Rasmus <333
