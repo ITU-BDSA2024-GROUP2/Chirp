ad8643f957feeecd9dfe0f8d8a41141a0ec75ddd
Rasmus Mygind Poulsen <rpou@itu.dk>
2024-09-04 14:56:00 +0200

    Added Chirp.cs Class.
    Co-authored-by: Mathias bimh@itu.dk
    Co-authored-by: Anders ageh@itu.dk

--- Chirp.CLI/Chirp.cs
+++ Chirp.CLI/Chirp.cs
@@ -2,6 +2,28 @@
 
 public class Chirp
 {
-    String Message
-    
+    private String message;
+    private String time;
+    private String author;
+
+    public Chirp(String message, String time, String author)
+    {
+        this.message = message;
+        this.time = time;
+        this.author = author;
+    }
+
+    public String GetMessage()
+    {
+        return message;
+    }
+    public String GetTime()
+    {
+        return time;
+    }
+    public String GetAuthor()
+    {
+        return author;
+    }
+
 }
