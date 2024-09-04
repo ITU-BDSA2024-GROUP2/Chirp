4fd9376302385f7ab30f0f8cc6f8c208b8af9060
Nikolai <nitn@itu.dk>
2024-09-04 15:17:11 +0200

    Fix dotnet version

---
+++ SimpleDB/CSVDatabase.cs
@@ -0,0 +1,14 @@
+﻿namespace SimpleDB;
+
+public sealed class CSVDatabase<T> : IDatabaseRepository<T>
+{
+    public IEnumerable<T> Read(int? limit = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Store(T record)
+    {
+        throw new NotImplementedException();
+    }
+}
