5762f765320f35e0f904c43e0aca7dd774acffcb
Nikolai <nitn@itu.dk>
2024-09-06 11:30:36 +0200

    Removed test code from main

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -8,10 +8,6 @@ class Program
     public static void Main(string[] args)
     {
         IDatabaseRepository<Cheep> database = new CSVDatabase<Cheep>();
-        Cheep cheep = new Cheep("nikolai", "New", 12345679);
-        database.Store(cheep);
-
-        //List<Cheep> cheeps = CSVParser.Parse(path);
         List<Cheep> cheeps = database.Read(20).ToList();
         UserInterface.PrintCheeps(cheeps);
     }
