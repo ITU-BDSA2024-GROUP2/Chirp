27969d953c5969a434ec922af86190d2ed2a7f23
Anders0106 <andersgfh@gmail.com>
2024-09-06 12:33:46 +0200

    Kan læse inputs med docopt

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -1,31 +1,44 @@
-﻿using SimpleDB;
+﻿using DocoptNet;
+
 using DocoptNet;
-
-const string usage = @"Chirp CLI version.
-
+using SimpleDB;
+ 
+namespace Chirp.CLI
+{
+    class Program
+    {
+        const string usage = @"Chirp CLI.
+ 
 Usage:
   chirp read <limit>
   chirp cheep <message>
   chirp (-h | --help)
   chirp --version
-
+ 
 Options:
   -h --help     Show this screen.
   --version     Show version.
 ";
-
-var arguments = new Docopt().Apply(usage, args, version: "1.0", exit: true)!;
-
-namespace Chirp.CLI
-{
-    class Program
-    {
-    
         public static void Main(string[] args)
         {
+            var arguments = new Docopt().Apply(usage, args, version: "1.0", exit: true)!;
+ 
+            string test = arguments["read"]?.ToString();    // "read" command
+            string test2 = arguments["<limit>"]?.ToString(); // <limit> argument
+
+            Console.WriteLine(test);  // Prints "True" if "read" command is present
+            Console.WriteLine(test2); // Prints the value of <limit>
+            
             IDatabaseRepository<Cheep> database = new CSVDatabase<Cheep>();
-            List<Cheep> cheeps = database.Read(20).ToList();
+
+
+            System.Collections.Generic.List<Cheep> cheeps = database.Read(20).ToList();
             UserInterface.PrintCheeps(cheeps);
+        }
+ 
+        private static void Cheep()
+        {
+ 
         }
     }
 }
