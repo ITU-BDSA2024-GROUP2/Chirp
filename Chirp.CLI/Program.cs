c294a2c50ea2978ee345e75662df978b69a6c5b6
Nikolai <nitn@itu.dk>
2024-09-06 08:46:45 +0200

    Implemented Read method for displaying cheeps form the database. Co-authored-by: Mads moes@itu.dk

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -7,13 +7,12 @@ class Program
     
     public static void Main(string[] args)
     {
-        string path = "data/chirp_cli_db.csv";
-        
         IDatabaseRepository<Cheep> database = new CSVDatabase<Cheep>();
-        Cheep cheep = new Cheep("nikolai", "test", 12345679);
+        Cheep cheep = new Cheep("nikolai", "New", 12345679);
         database.Store(cheep);
 
-        List<Cheep> cheeps = CSVParser.Parse(path);
+        //List<Cheep> cheeps = CSVParser.Parse(path);
+        List<Cheep> cheeps = database.Read(20).ToList();
         UserInterface.PrintCheep(cheeps);
     }
 }
