80e7fcc47bee0387998689f0b86f82a69d819b50
Nikolai <nitn@itu.dk>
2024-09-06 11:42:51 +0200

    Installed docopt package

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -1,14 +1,31 @@
 ﻿using SimpleDB;
+using DocoptNet;
 
-namespace Chirp.CLI;
+const string usage = @"Chirp CLI version.
 
-class Program
+Usage:
+  chirp read <limit>
+  chirp cheep <message>
+  chirp (-h | --help)
+  chirp --version
+
+Options:
+  -h --help     Show this screen.
+  --version     Show version.
+";
+
+var arguments = new Docopt().Apply(usage, args, version: "1.0", exit: true)!;
+
+namespace Chirp.CLI
 {
-    
-    public static void Main(string[] args)
+    class Program
     {
-        IDatabaseRepository<Cheep> database = new CSVDatabase<Cheep>();
-        List<Cheep> cheeps = database.Read(20).ToList();
-        UserInterface.PrintCheeps(cheeps);
+    
+        public static void Main(string[] args)
+        {
+            IDatabaseRepository<Cheep> database = new CSVDatabase<Cheep>();
+            List<Cheep> cheeps = database.Read(20).ToList();
+            UserInterface.PrintCheeps(cheeps);
+        }
     }
 }
