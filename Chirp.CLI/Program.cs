fdc2fe3ebe54c47869ffc4d13337d79ae1298e66
Nikolai <nitn@itu.dk>
2024-09-09 10:46:46 +0200

    Docopt refactoring Co-authored-by: Mathias <bimh@itu.dk> Co-authored-by: Mads <moes@itu.dk>

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -19,17 +19,32 @@ Options:
 ";
 
         private static IDatabaseRepository<Cheep> database = new CSVDatabase<Cheep>();
+        
+        static int ShowHelp(string help) { Console.WriteLine(help); return 0; }
+        static int ShowVersion(string version) { Console.WriteLine(version); return 0; }
+        static int OnError(string usage) { Console.Error.WriteLine(usage); return 1; }
 
         public static void Main(string[] args)
         {
-            var arguments = new Docopt().Apply(usage, args, version: "Chirp 1.0", exit: true)!;
+            // https://docopt.github.io/docopt.net/dev/#api
+            
+            Docopt.CreateParser(usage)
+                .WithVersion("Chirp 1.0")
+                .Parse(args)
+                .Match(Run,
+                    result => ShowHelp(result.Help),
+                    result => ShowVersion(result.Version),
+                    result => OnError(result.Usage));
+        }
 
+        public static int Run(IDictionary<string, ArgValue> arguments)
+        {
             if (arguments["read"].IsTrue)
             {
                 if (!int.TryParse(arguments["<limit>"].ToString(), out int limit) || limit < 1)
                 {
                     Console.WriteLine("Bad input: <limit> must be an integer greater than 0.");
-                    return;
+                    return 1;
                 }
                 RunReadCommand(limit);
             }
@@ -38,6 +53,7 @@ Options:
                 string message = arguments["<message>"].ToString();
                 RunCheepCommand(message);
             }
+            return 0;
         }
 
         private static void RunReadCommand(int limit)
