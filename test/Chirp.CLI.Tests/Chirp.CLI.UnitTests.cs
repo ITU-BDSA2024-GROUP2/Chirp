aa5f82483f32246acc329e573a7b64f2fa0b3c66
Nikolai <nitn@itu.dk>
2024-09-13 08:28:27 +0200

    Added example unit test class

---
+++ test/Chirp.CLI.Tests/Chirp.CLI.UnitTests.cs
@@ -0,0 +1,20 @@
+using Xunit;
+
+namespace Chirp.CLI.UnitTests
+{
+    public class Chirp_CLI_UnitTests
+    {
+        [Fact]
+        public void TestName()
+        {
+            // Arrange
+            
+            // Act
+            
+            // Assert
+            
+            Assert.True(true);
+            
+        }
+    }
+}
