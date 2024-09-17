705f06ddaacc0ee349a6e34812925c77930fd02f
Rasmus Mygind Poulsen <rpou@itu.dk>
2024-09-17 14:44:18 +0200

    Moved integration test and e2e test to proper place

--- test/Chirp.CLI.Tests/Chirp.CLI.End2endTests.cs
+++ test/Chirp.CLI.Tests/Chirp.CLI.End2endTests.cs
@@ -3,4 +3,27 @@ namespace DefaultNamespace;
 public class Chirp_CLI_End2endTests
 {
     
+    [Fact]
+    public void CsvToCheepInConsole() //tror den er e2e
+    {
+        // Arrange
+        string path = "../../../../../data/CsvParseTest.csv";
+        
+        // Act
+        var cheeps = CSVParser.Parse<Cheep>(path);
+        using (var consoleOutput = new StringWriter())
+        {
+            Console.SetOut(consoleOutput);
+
+
+            // Act
+            UserInterface.PrintCheeps(cheeps);
+            var outputLines = consoleOutput.ToString().Trim().Split(Environment.NewLine); //source: https://stackoverflow.com/a/22878533 .newLine sikrer at det virker både på mac og windows
+            
+
+            // Assert
+            Assert.Equal("ageh @ 01/08/23 14:09:20: SIIIIIUUUUUUUU!", outputLines[0].Trim());
+            Assert.Equal("nitn @ 02/08/23 14:19:38: Recently engaged", outputLines[1].Trim());
+        }
+    }
 }
--- test/Chirp.CLI.Tests/Chirp.CLI.UnitTests.cs
+++ test/Chirp.CLI.Tests/Chirp.CLI.UnitTests.cs
@@ -61,29 +61,6 @@ public class Chirp_CLI_UnitTests
         Assert.Equal(expectedCheep, cheep);
     }
         
-   
-    [Theory]
-    [InlineData("Michael", "I have a ball", 1690891760)]
-    [InlineData("Harald", "I am him", 1726056884)]
-    [InlineData("Anders", "I have Netflix", 1726056833)]
-    public void PrintingCheepsTest(string author, string message, long timestamp)
-    {
-        // Arrange
-        Cheep cheep = new Cheep(author, message, timestamp);
-        List<Cheep> cheeps = new List<Cheep> { cheep };
-        using (var consoleOutput = new StringWriter())
-        {
-            Console.SetOut(consoleOutput);
-
-
-            // Act
-            UserInterface.PrintCheeps(cheeps);
-            string result = cheep.ToString();
-
-            // Assert
-            Assert.Equal(result, consoleOutput.ToString().Trim()); 
-        }
-    }
         
     [Fact]
     public void CsvParserTest()
@@ -101,28 +78,4 @@ public class Chirp_CLI_UnitTests
         Assert.Equal(cheep1.Timestamp, 1690891760);
     }
         
-        
-    [Fact]
-    public void CsvToCheepInConsole() //tror den er e2e
-    {
-        // Arrange
-        string path = "../../../../../data/CsvParseTest.csv";
-        
-        // Act
-        var cheeps = CSVParser.Parse<Cheep>(path);
-        using (var consoleOutput = new StringWriter())
-        {
-            Console.SetOut(consoleOutput);
-
-
-            // Act
-            UserInterface.PrintCheeps(cheeps);
-            var outputLines = consoleOutput.ToString().Trim().Split(Environment.NewLine); //source: https://stackoverflow.com/a/22878533 .newLine sikrer at det virker både på mac og windows
-            
-
-            // Assert
-            Assert.Equal("ageh @ 01/08/23 14:09:20: SIIIIIUUUUUUUU!", outputLines[0].Trim());
-            Assert.Equal("nitn @ 02/08/23 14:19:38: Recently engaged", outputLines[1].Trim());
-    	}
-	}
 }
