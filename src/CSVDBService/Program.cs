a9b351d0a8d4b76f2f38105167e8641fe5ac6426
Nikolai <nitn@itu.dk>
2024-09-18 15:13:28 +0200

    Added project reference to CSVDBService
    Co-authored-by Mads <moes@itu.dk>

--- src/CSVDBService/Program.cs
+++ src/CSVDBService/Program.cs
@@ -1,6 +1,9 @@
+using Chirp.CLI;
+using CSVDB;
+    
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapPost("/cheep", (Cheep cheep) => "Hello World!");
 
 app.Run();
