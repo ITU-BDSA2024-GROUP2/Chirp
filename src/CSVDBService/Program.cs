e7811c6226cf9d4c702fc4d357260ebb48dc7333
Matbhansen <maitrox@live.com>
2024-09-20 09:36:13 +0200

    update

--- src/CSVDBService/Program.cs
+++ src/CSVDBService/Program.cs
@@ -1,13 +1,6 @@
-using Chirp.CLI;
-using CSVDB;
-
-var database = CSVDatabase<Cheep>.Instance;
-
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/cheeps", () => database.Read());
-
-app.MapPost("/cheep", (Cheep cheep) => database.Store(cheep));
+app.MapGet("/", () => "Hello World!");
 
 app.Run();
