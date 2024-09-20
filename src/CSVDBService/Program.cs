790f0543be9132a9ee8ef69cb324500132088b6e
Anders0106 <andersgfh@gmail.com>
2024-09-20 11:00:53 +0200

    reversed hello world to cheeps again

--- src/CSVDBService/Program.cs
+++ src/CSVDBService/Program.cs
@@ -6,7 +6,8 @@ var database = CSVDatabase<Cheep>.Instance;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello world!");
-//app.MapPost("/cheep", (Cheep cheep) => database.Store(cheep));
+app.MapGet("/cheeps", () => database.Read());
+
+app.MapPost("/cheep", (Cheep cheep) => database.Store(cheep));
 
 app.Run();
