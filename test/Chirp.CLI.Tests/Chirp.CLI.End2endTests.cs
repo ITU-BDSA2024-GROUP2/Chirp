6b132b440aa5450517b1524f0ed4da4a3fb8912a
Rasmus Mygind Poulsen <rpou@itu.dk>
2024-09-20 09:52:29 +0200

    Modified E2ETest so that it works with LocalHost Co-authored-by: Mads moes@itu.dk

--- test/Chirp.CLI.Tests/Chirp.CLI.End2endTests.cs
+++ test/Chirp.CLI.Tests/Chirp.CLI.End2endTests.cs
@@ -15,65 +15,17 @@ public class Chirp_CLI_End2endTests
     public async Task WebService_CheepCommand()
     {
         // Arrange
-        var newCheep = Cheep.CreateCheep("Hello from Web Service!");
+        var newCheep = Cheep.CreateCheep("I am a TEST!!");
         
         // Act
-        // Send a POST request to /cheep to store the cheep
         HttpResponseMessage postResponse = await Client.PostAsJsonAsync("/cheep", newCheep);
-        postResponse.EnsureSuccessStatusCode();
-
-        // Send a GET request to /cheeps to retrieve the stored cheeps
+        
         var cheeps = await Client.GetFromJsonAsync<List<Cheep>>("/cheeps");
 
         // Assert
         Assert.NotNull(cheeps);
-        Assert.Contains(cheeps, c => c.Message == "Hello from Web Service!" && c.Author == Environment.UserName);
-    }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    /*
-     private readonly string directoryPath = "../../../../../src/Chirp.CLI";
-     
-    private readonly string csvPath = "../../../../../data/chirp_cli_db.csv";
-    
-    [Fact]
-    public void CSVDB_cheepCommand()
-    {
-        string command = "cheep Hello!!!";
+        Assert.Contains(cheeps, c => c.Message == "I am a TEST!!" && c.Author == Environment.UserName);
+        Assert.True(postResponse.IsSuccessStatusCode);
         
-        var process = new Process
-        {
-            StartInfo = new ProcessStartInfo
-            {
-                FileName = "dotnet",
-                WorkingDirectory = @$"{directoryPath}",
-                Arguments = $"run {command}",
-                RedirectStandardOutput = true,
-                UseShellExecute = false,
-                CreateNoWindow = true
-            }
-        };
-        
-        process.Start();
-        string output = process.StandardOutput.ReadToEnd();
-        process.WaitForExit();
-
-        CSVDatabase<Cheep>.Instance.SetFilePath(csvPath);
-        var cheeps = CSVDatabase<Cheep>.Instance.Read();
-        var lastCheep = cheeps.LastOrDefault(); 
-
-        Assert.NotNull(lastCheep);
-        Assert.Equal("Hello!!!", lastCheep.Message);
-        Assert.Equal(Environment.UserName, lastCheep.Author);
     }
-    */
 }
