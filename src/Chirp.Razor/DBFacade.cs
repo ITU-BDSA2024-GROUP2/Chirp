ed4f6de2ade59b07089ae4897f93fdbd6b63daed
Nikolai <nitn@itu.dk>
2024-09-27 09:25:35 +0200

    Created DBFacade Class and method for querying author, text and pubDate from the database

--- src/Chirp.Razor/DBFacade.cs
+++ src/Chirp.Razor/DBFacade.cs
@@ -1,25 +1,53 @@
+using System.Data;
 using Microsoft.Data.Sqlite;
+
+namespace Chirp.Razor;
 
 public class DBFacade
 {
-    private readonly string sqlDBFilePath = "/tmp/chirp.db";
+    private readonly string _sqlDBFilePath;
 
-    public List<CheepViewModel> GetCheeps()
+    public DBFacade(string sqlDBFilePath = "/tmp/chirp.db")
     {
-        var sqlQuery = @"SELECT * FROM message ORDER by message.pub_date desc";
+        _sqlDBFilePath = sqlDBFilePath;
+    }
 
-        using (var connection = new SqliteConnection($"Data Source={sqlDBFilePath}"))
+    public List<CheepViewModel> ReadCheeps() // Skal denne laves async??
+    {
+        List<CheepViewModel> cheeps = new List<CheepViewModel>();
+        
+        var queryString = @"SELECT u.username, m.text, m.pub_date
+                            FROM message m 
+                            JOIN user u ON m.author_id = u.user_id
+                            ORDER BY m.pub_date DESC";
+        
+        using (var connection = new SqliteConnection($"Data Source={_sqlDBFilePath}"))
         {
             connection.Open();
-
+            
             var command = connection.CreateCommand();
-            command.CommandText = sqlQuery;
-
+            command.CommandText = queryString;
+            
             using var reader = command.ExecuteReader();
             while (reader.Read())
             {
-                ...
+                var dataRecord = (IDataRecord)reader;
+
+                var cheep = ReadSingleRow(dataRecord);
+                cheeps.Add(cheep);
             }
+            reader.Close();
         }
+        return cheeps;
+    }
+    
+    private static CheepViewModel ReadSingleRow(IDataRecord dataRecord)
+    {
+        var author = dataRecord["username"].ToString();
+        var message = dataRecord["text"].ToString();
+        var pubDate = Convert.ToInt64(dataRecord["pub_date"]);
+        
+        var Cheep = CheepViewModel.CreateCheep(author, message, pubDate);
+        return Cheep;
     }
 }
