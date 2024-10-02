4e533f224f1840d305b1f948e42562ebfe24b019
Anders0106 <andersgfh@gmail.com>
2024-10-02 14:46:46 +0200

    CheepDTO class

---
+++ src/Chirp.Razor/CheepDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Chirp.Razor;
+
+public class CheepDTO
+{
+    public string Message { get; set; }
+    public string Timestamp { get; set; }
+    public string Author { get; set; }
+    
+    
+}
