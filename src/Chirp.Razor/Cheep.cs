e78f3cf821d258bddafeb4bda7981b5aa5a41b89
Nikolai <nitn@itu.dk>
2024-10-02 15:18:33 +0200

    Added DbInitializer class from project description. Co-authored-by: Anders ageh@itu.dk Co-authored-by: Mads moes@itu.dk Co-authored-by: Mathias bimh@itu.dk Co-authored-by: Rasmus rpou@itu.dk

--- src/Chirp.Razor/Cheep.cs
+++ src/Chirp.Razor/Cheep.cs
@@ -6,4 +6,5 @@ public class Cheep
     public string Text { get; set; }
     public DateTime TimeStamp { get; set; }
     public Author Author { get; set; }
+    public int AuthorId { get; set; }
 }
