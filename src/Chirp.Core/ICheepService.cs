aa86fdf06764c8084c9921eba834b023fe7a0773
Nikolai <nitn@itu.dk>
2024-10-09 14:02:31 +0200

    Onion test

---
+++ src/Chirp.Core/ICheepService.cs
@@ -0,0 +1,7 @@
+namespace Chirp.Razor;
+
+public interface ICheepService 
+{
+    public Task<List<CheepDTO>> GetCheeps(int currentPage);
+    public Task<List<CheepDTO>> GetCheepsFromAuthor(string authorName, int currentPage);
+}
--- src/Chirp.Infrastucture/CheepService.cs
+++ src/Chirp.Infrastucture/CheepService.cs
@@ -1,11 +1,4 @@
 using Chirp.Razor;
-
-public interface ICheepService 
-{
-    public Task<List<CheepDTO>> GetCheeps(int currentPage);
-    public Task<List<CheepDTO>> GetCheepsFromAuthor(string authorName, int currentPage);
-}
-
 public class CheepService : ICheepService
 {
 
