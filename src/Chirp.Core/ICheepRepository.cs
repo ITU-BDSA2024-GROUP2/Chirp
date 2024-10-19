17b304d02a11b73b9033cfdccb94fda1569e35e1
Nikolai <nitn@itu.dk>
2024-10-19 15:53:31 +0200

    added new methods in CheepRepository to ICheepRepository

--- src/Chirp.Core/ICheepRepository.cs
+++ src/Chirp.Core/ICheepRepository.cs
@@ -4,4 +4,7 @@ public interface ICheepRepository
 {
     public Task<List<CheepDTO>> GetCheeps(int currentPage);
     public Task<List<CheepDTO>> GetCheepsFromAuthor(string authorName, int currentPage);
+    public Task<Author> CreateAuthor(AuthorDTO authorDto);
+    public Task<Cheep> CreateCheep(CheepDTO cheepDto, AuthorDTO authorDto);
+    public Task<Author> FindAuthor(AuthorDTO authorDto);
 }
