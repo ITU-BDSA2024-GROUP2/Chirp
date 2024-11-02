9e9304f2347f1e51cf14297442bd8c2c77faed48
Matbhansen <maitrox@live.com>
2024-11-02 15:09:03 +0100

    Added a write cheep EndToEndTest

--- tests/Chirp.UI.Tests/EndToEndTest.cs
+++ tests/Chirp.UI.Tests/EndToEndTest.cs
@@ -87,5 +87,60 @@ namespace Chirp.UI.Tests
             //
             //await Expect(Page.GetByText("invalid login")).ToBeVisibleAsync();
         }
+
+        [Test]
+        public async Task UserResgistersANewAccountAndLogsInWithNewAccountWritesCheepDeletesAccount()
+        {   
+            //Arrange
+            await Page.GotoAsync("http://localhost:5273/");
+            
+            //Act
+            await Page.GetByRole(AriaRole.Link, new() { Name = "register" }).ClickAsync();
+            await Page.GetByPlaceholder("user name").ClickAsync();
+            await Page.GetByPlaceholder("user name").FillAsync("ATestUser");
+            await Page.GetByPlaceholder("name@example.com").ClickAsync();
+            await Page.GetByPlaceholder("name@example.com").FillAsync("testmail@mail.com");
+            await Page.GetByLabel("Password", new() { Exact = true }).ClickAsync();
+            await Page.GetByLabel("Password", new() { Exact = true }).FillAsync("Testpassword123!");
+            await Page.GetByLabel("Confirm Password").ClickAsync();
+            await Page.GetByLabel("Confirm Password").FillAsync("Testpassword123!");
+            await Page.GetByRole(AriaRole.Button, new() { Name = "Register" }).ClickAsync();
+
+            //Assert
+            await Expect(Page.GetByText("Register confirmation")).ToBeVisibleAsync();
+
+            //Act
+            await Page.GetByRole(AriaRole.Link, new() { Name = "Click here to confirm your" }).ClickAsync();
+            await Page.GetByRole(AriaRole.Link, new() { Name = "login" }).ClickAsync();
+            await Page.GetByPlaceholder("name@example.com").ClickAsync();
+            await Page.GetByPlaceholder("name@example.com").FillAsync("testmail@mail.com");
+            await Page.GetByPlaceholder("password").ClickAsync();
+            await Page.GetByPlaceholder("password").FillAsync("Testpassword123!");
+            await Page.GetByLabel("Remember me?").CheckAsync();
+            await Page.GetByRole(AriaRole.Button, new() { Name = "Log in" }).ClickAsync();
+            
+            //Assert
+            await Expect(Page.GetByText("TestUser")).ToBeVisibleAsync();
+
+            //Act
+
+
+            //Act
+            await Page.GetByRole(AriaRole.Link, new() { Name = "manage account" }).ClickAsync();
+            await Page.GetByRole(AriaRole.Link, new() { Name = "Personal data" }).ClickAsync();
+            await Page.GetByRole(AriaRole.Button, new() { Name = "Delete" }).ClickAsync();
+            await Page.GetByPlaceholder("Please enter your password.").ClickAsync();
+            await Page.GetByPlaceholder("Please enter your password.").FillAsync("Testpassword123!");
+            await Page.GetByRole(AriaRole.Button, new() { Name = "Delete data and close my" }).ClickAsync();
+            await Page.GetByRole(AriaRole.Link, new() { Name = "login" }).ClickAsync();
+            await Page.GetByPlaceholder("name@example.com").ClickAsync();
+            await Page.GetByPlaceholder("name@example.com").FillAsync("test@mail.com");
+            await Page.GetByPlaceholder("password").ClickAsync();
+            await Page.GetByPlaceholder("password").FillAsync("Testpassword123!");
+            await Page.GetByRole(AriaRole.Button, new() { Name = "Log in" }).ClickAsync();
+
+            //
+            //await Expect(Page.GetByText("invalid login")).ToBeVisibleAsync();
+        }
     }
 }
