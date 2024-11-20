cca9ae5b0d33e41cc25cdd3124487f359fa51360
Rasmus Mygind Poulsen <rpou@itu.dk>
2024-11-20 16:05:22 +0100

    better username catch Co-authored-by: Mathias bimh@itu.dk Co-authored-by: Nikolai nitn@itu.dk

--- src/Chirp.Web/Areas/Identity/Pages/Account/AboutMe/InfoPage.cshtml.cs
+++ src/Chirp.Web/Areas/Identity/Pages/Account/AboutMe/InfoPage.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using Chirp.Core;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Chirp.Web.Areas.Identity.Pages.Account.AboutMe;
@@ -7,12 +8,24 @@ namespace Chirp.Web.Areas.Identity.Pages.Account.AboutMe;
 public class InfoPage : PageModel
 {
     private readonly SignInManager<Author> _signInManager;
-    
     public string ProviderDisplayName { get; set; }
+    public string LoginProvider { get; set; }
     
-    public async void OnGet()
+    public InfoPage(SignInManager<Author> signInManager)
+    {
+          _signInManager = signInManager;
+          
+    }
+    
+    public async Task<ActionResult> OnGet()
     {
         var info = await _signInManager.GetExternalLoginInfoAsync();
+        Console.WriteLine("PRINT STATEMENT START------------");
+        Console.WriteLine(info.LoginProvider);
+        Console.WriteLine("PRINT STATEMENT END------------");
         ProviderDisplayName = info.ProviderDisplayName;
+        LoginProvider = info.LoginProvider;
+        
+        return Page();
     }
 }
