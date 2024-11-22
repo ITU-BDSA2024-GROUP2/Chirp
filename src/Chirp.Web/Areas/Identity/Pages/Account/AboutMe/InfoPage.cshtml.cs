46ee2eb8d32f9c5439e4ff291ecdc677500ca86a
Rasmus Mygind Poulsen <rpou@itu.dk>
2024-11-22 11:50:50 +0100

    Aboutme page done (No style) Co-authored-by: Mathias bimh@itu.dk

--- src/Chirp.Web/Areas/Identity/Pages/Account/AboutMe/InfoPage.cshtml.cs
+++ src/Chirp.Web/Areas/Identity/Pages/Account/AboutMe/InfoPage.cshtml.cs
@@ -1,4 +1,6 @@
-﻿using Chirp.Core;
+﻿using System.Collections;
+using Chirp.Core;
+using Chirp.Web.Pages;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -7,15 +9,33 @@ namespace Chirp.Web.Areas.Identity.Pages.Account.AboutMe
 {
     public class InfoPage : PageModel
     {
+        private readonly ICheepRepository _cheepRepository;
+        private readonly IAuthorRepository _authorRepository;
+
+        public Dictionary<string, bool> FollowerMap;
+        private int _currentPage;
+        public List<CheepDTO> Cheeps { get; set; }
+
+        [BindProperty]
+        public CheepViewModel CheepInput { get; set; }
+
+        public ICollection<Author> following;
+        
         private readonly UserManager<Author> _userManager;
         public string LoginProvider { get; set; } = "Default";
 
-        public InfoPage(UserManager<Author> userManager)
+        
+        
+        public InfoPage(UserManager<Author> userManager, ICheepRepository cheepRepository, IAuthorRepository authorRepository)
         {
+            _cheepRepository = cheepRepository;
+            _authorRepository = authorRepository;
+            
+            
             _userManager = userManager;
         }
 
-        public async Task<IActionResult> OnGet()
+        public async Task<IActionResult> OnGet([FromQuery] int? page)
         {
             var user = await _userManager.GetUserAsync(User);
             if (user == null)
@@ -30,8 +50,27 @@ namespace Chirp.Web.Areas.Identity.Pages.Account.AboutMe
                 var externalLogin = logins.First();
                 LoginProvider = externalLogin.LoginProvider;
             }
+            
+            _currentPage = page ?? 1;
+
+            var username = User.Identity!.Name!;
+            var author = await _cheepRepository.FindAuthorByName(username);
+            
+        
+            Cheeps = await GetAuthorCheeps(username, _currentPage);
+
+
+            following = user.Following;
 
             return Page();
         }
+        
+        private async Task<List<CheepDTO>> GetAuthorCheeps(string author, int page)
+        {
+            Cheeps = await _cheepRepository.GetCheepsFromAuthor(author, page);
+            return Cheeps;
+        }
+        
+        
     }
 }
