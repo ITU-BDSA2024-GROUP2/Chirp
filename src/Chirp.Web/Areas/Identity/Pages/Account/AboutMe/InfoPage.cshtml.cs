4b82e4fb67db331b54e447138aeca5d22244fa0d
Rasmus Mygind Poulsen <rpou@itu.dk>
2024-11-22 10:33:13 +0100

    Loginprovider Co-authored-by: Mathias bimh@itu.dk

--- src/Chirp.Web/Areas/Identity/Pages/Account/AboutMe/InfoPage.cshtml.cs
+++ src/Chirp.Web/Areas/Identity/Pages/Account/AboutMe/InfoPage.cshtml.cs
@@ -3,29 +3,40 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Chirp.Web.Areas.Identity.Pages.Account.AboutMe;
-
-public class InfoPage : PageModel
+namespace Chirp.Web.Areas.Identity.Pages.Account.AboutMe
 {
-    private readonly SignInManager<Author> _signInManager;
-    public string ProviderDisplayName { get; set; }
-    public string LoginProvider { get; set; }
-    
-    public InfoPage(SignInManager<Author> signInManager)
+    public class InfoPage : PageModel
     {
-          _signInManager = signInManager;
-          
-    }
-    
-    public async Task<ActionResult> OnGet()
-    {
-        /*var info = await _signInManager.GetExternalLoginInfoAsync();
-        Console.WriteLine("PRINT STATEMENT START------------");
-        Console.WriteLine(info.LoginProvider);
-        Console.WriteLine("PRINT STATEMENT END------------");
-        ProviderDisplayName = info.ProviderDisplayName;
-        LoginProvider = info.LoginProvider;
-        */
-        return Page();
+        private readonly SignInManager<Author> _signInManager;
+
+        public string ProviderDisplayName { get; set; } = "N/A";
+        public string LoginProvider { get; set; } = "Default";
+
+        public InfoPage(SignInManager<Author> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        public async Task<ActionResult> OnGet()
+        {
+            var info = await _signInManager.GetExternalLoginInfoAsync();
+
+            Console.WriteLine("PRINT STATEMENT START------------");
+            if (info != null)
+            {
+                Console.WriteLine($"LoginProvider: {info.LoginProvider}");
+                Console.WriteLine($"ProviderDisplayName: {info.ProviderDisplayName}");
+
+                ProviderDisplayName = info.ProviderDisplayName ?? "N/A";
+                LoginProvider = info.LoginProvider ?? "Default";
+            }
+            else
+            {
+                Console.WriteLine("No external login information available.");
+            }
+            Console.WriteLine("PRINT STATEMENT END------------");
+
+            return Page();
+        }
     }
 }
