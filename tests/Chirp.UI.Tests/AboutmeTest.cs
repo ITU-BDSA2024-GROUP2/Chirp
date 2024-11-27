464b0546f3eefbff14a8ea5650dfa8baec1e3462
Rasmus Mygind Poulsen <rpou@itu.dk>
2024-11-27 11:24:17 +0100

    AboutMeTEst is visible with and without login

--- tests/Chirp.UI.Tests/AboutmeTest.cs
+++ tests/Chirp.UI.Tests/AboutmeTest.cs
@@ -44,23 +44,43 @@ public class AboutmeTest : PageTest
     }
     
     [Test]
-    public async Task CheepBoxIsNotVisibleWhenNotLoggedIn()
+    public async Task AboutMePageVisableWhenLoggedIn()
+    {
+        // Arrange
+        await Page.GotoAsync("https://localhost:5273/Identity/Account/AboutMe/InfoPage");
+        
+        // Assert
+        await Expect(Page).ToHaveTitleAsync(new Regex("About me"));
+    }
+    
+    [Test]
+    public async Task AboutMePageNotVisableWhenNotLoggedIn()
+    {
+        // Arrange
+        var isUserLoggedIn = await Page.GetByRole(AriaRole.Link, new() { Name = "my timeline" }).IsVisibleAsync();
+        if (isUserLoggedIn)
+        {
+            await Page.GetByRole(AriaRole.Button, new() { Name = "logout [username]" }).ClickAsync();
+        }
+        
+        await Page.GotoAsync("https://localhost:5273/Identity/Account/AboutMe/InfoPage");
+        
+        // Assert
+        await Expect(Page).ToHaveTitleAsync("Log in");
+    }
+    
+    [Test]
+    public async Task AboutMePageDisplayUsername()
     {
         // Arrange
         await Page.GotoAsync("https://localhost:5273/Identity/Account/AboutMe/InfoPage");
         
         // Act
-        
-        //var listItemText = await Page.Locator("li").TextContentAsync();
-        
-        var listItemText = await Page.Locator("ul li").TextContentAsync();
-
-        // Extract the username using a regular expression
-        var match = Regex.Match(listItemText, @"Your Username is: (.+)");
+        var listItemText = Page.Locator("div.body ul > div > li");
+        await Page.GetByRole(AriaRole.Link, new() { Name = "about me" }).ClickAsync();
             
         // Assert
-        //Assert.Equals("user name", match);
-        //Assert.Equals(listItemText., match);
+        await Expect(listItemText.Nth(0)).ToHaveTextAsync("Your Username is: username");
     }
     
     // This method can be used to prepare a test that requires a logged in user
@@ -111,7 +131,7 @@ public class AboutmeTest : PageTest
         
         await Page.GetByRole(AriaRole.Link, new() { Name = "manage account" }).ClickAsync();
         await Page.GetByRole(AriaRole.Link, new() { Name = "Personal data" }).ClickAsync();
-        await Page.GetByRole(AriaRole.Button, new() { Name = "Delete" }).ClickAsync();
+        await Page.GetByRole(AriaRole.Link, new() { Name = "Delete" }).ClickAsync();
         await Page.GetByPlaceholder("Please enter your password.").FillAsync("Password123!");
         await Page.GetByRole(AriaRole.Button, new() { Name = "Delete data and close my" }).ClickAsync();
     }
