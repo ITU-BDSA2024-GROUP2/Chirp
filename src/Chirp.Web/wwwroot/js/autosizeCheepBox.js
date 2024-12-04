8010e36c92c2aa8b68add9b663e4bc9bc4eed8bd
Anders0106 <andersgfh@gmail.com>
2024-12-04 12:56:12 +0100

    scripts til sin egen mappe, optimeret cheeps og cheepbox

---
+++ src/Chirp.Web/wwwroot/js/autosizeCheepBox.js
@@ -0,0 +1,10 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const messageBox = document.getElementById("messageBox");
+
+    if (messageBox) {
+        messageBox.addEventListener("input", function () {
+            this.style.height = "auto"; 
+            this.style.height = (this.scrollHeight) + "px"; 
+        });
+    }
+});
