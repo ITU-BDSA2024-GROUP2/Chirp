2e110d90e6a7669846c1bf1b75f523865e4ba995
Matbhansen <maitrox@live.com>
2024-12-06 11:08:06 +0100

    Added doc to like

--- src/Chirp.Core/Like.cs
+++ src/Chirp.Core/Like.cs
@@ -1,5 +1,10 @@
 namespace Chirp.Core;
 
+/// <summary>
+/// This class repesents likes on a cheep.
+/// A user can only like the same cheep once.
+/// A user can also remove their on a liked cheep.
+/// </summary>
 public class Like
 {
     public string LikeId { get; set; } = Guid.NewGuid().ToString();
