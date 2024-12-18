83198f122c849200dc9be3a6b3a6033af7709949
MadsOstrup <moes@itu.dk>
2024-12-18 11:13:26 +0100

    doc for ICheepRepository and IAuthorRepository

--- src/Chirp.Core/IAuthorRepository.cs
+++ src/Chirp.Core/IAuthorRepository.cs
@@ -1,13 +1,14 @@
 namespace Chirp.Core;
 
+/// <summary>
+/// The IAuthorRepository interface defines the methods that the AuthorRepository class must implement.
+/// </summary>
 public interface IAuthorRepository
 {
     public Task<Author> CreateAuthor(AuthorDTO authorDto);
     public Task<Author> FindAuthor(string authorName);
     public Task Follow(string userName, string authorName);
-    
     public Task Unfollow(string userName, string authorName);
-
     public Task<Boolean> IsFollowing(string userName, string authorName);
     public Task<List<string>> GetFollowing(string userName);
     public Task<List<string>> GetFollowers(string userName);
